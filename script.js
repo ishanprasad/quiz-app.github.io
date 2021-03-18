a200cfbf448595efdf2574ca0b87963cd66b7709
Ishan Prasad <rockjohnson249@gmail.com>
2021-03-19 01:01:47 +0530

    Quiz App
    Using HTML, CSS and JavaScript.

---
+++ script.js
@@ -0,0 +1,92 @@
+const quizData = [
+    {
+        question: "What is the real name of the Undertaker",
+        a: "Mark Galloway",
+        b: "William Callaway",
+        c: "Mark Calaway",
+        d: "None of the above",
+        correct: "c",
+    },
+    {
+        question: "Name the finishing move of the Nature Boy",
+        a: "The Jackhammer",
+        b: "S-T-F",
+        c: "Figure-4 leg lock",
+        d: "Sister Abigail",
+        correct: "c",
+    },
+    {
+        question: "How old is the WWE?",
+        a: "39",
+        b: "40",
+        c: "41",
+        d: "42",
+        correct: "b",
+    },
+];
+
+const quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+
+let currentQuiz = 0;
+let score = 0;
+
+loadQuiz();
+
+function loadQuiz() {
+    deselectAnswers();
+
+    const currentQuizData = quizData[currentQuiz];
+
+    questionEl.innerText = currentQuizData.question;
+    a_text.innerText = currentQuizData.a;
+    b_text.innerText = currentQuizData.b;
+    c_text.innerText = currentQuizData.c;
+    d_text.innerText = currentQuizData.d;
+}
+
+function getSelected() {
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id;
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+submitBtn.addEventListener("click", () => {
+    // check to see the answer
+    const answer = getSelected();
+
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++;
+        }
+
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz();
+        } else {
+            quiz.innerHTML = `
+                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
+                
+                <button onclick="location.reload()">Reload</button>
+            `;
+        }
+    }
+});
